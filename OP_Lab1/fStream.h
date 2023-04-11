9b2ccb1034cd54fce55e37ad51e11503f4eb8970
MichaelTkach <130134717+MichaelTkach@users.noreply.github.com>
2023-04-11 16:45:42 +0300

    Create fStream.h

---
+++ OP_Lab1/fStream.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+void printFileContents(string& filename);
+
+void appendToFile(string& filename);
+
+void writeWordsToFile(string& inputFile, string& outputFile);
+
+void removeDuplicateFile(string& inputFilename);
+
+void findMostCommonFile(string& inputFilename);
+
+void createFile1();
